2f4f12d6715104a91a3a9f8178ced6867f7b3ae5
Agus Kurniawan <agusk@users.noreply.github.com>
2023-11-26 17:47:58 +0700

    initial

---
+++ helloapi/src/test/java/id/ilmudata/helloapi/HelloapiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ilmudata.helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
